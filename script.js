f98a0caaa9318cc17cfaf1859ad137c5193f827f
Sergei T <62507867+SergeiTsikhan@users.noreply.github.com>
2021-01-20 14:56:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener('keypress', function (e) {
+    if (e.code === 'KeyA'){
+        let aSound = new Audio('audio/A.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyS'){
+        let aSound = new Audio('audio/S.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyD'){
+        let aSound = new Audio('audio/D.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyF'){
+        let aSound = new Audio('audio/F.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyG'){
+        let aSound = new Audio('audio/G.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyH'){
+        let aSound = new Audio('audio/H.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyJ'){
+        let aSound = new Audio('audio/J.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyW'){
+        let aSound = new Audio('audio/W.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyE'){
+        let aSound = new Audio('audio/E.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyT'){
+        let aSound = new Audio('audio/T.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyY'){
+        let aSound = new Audio('audio/Y.mp3');
+        return aSound.play();
+    }
+    if (e.code === 'KeyU'){
+        let aSound = new Audio('audio/U.mp3');
+        return aSound.play();
+    }
+});
